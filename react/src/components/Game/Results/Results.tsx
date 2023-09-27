7e66376aec7b09b1062317c78696bda28d4b66a7
mahererais <raismaher.store@gmail.com>
2023-09-27 12:00:56 +0200

    add color to result game stat & move react files

--- react/src/components/Game/Results/Results.tsx
+++ react/src/components/Game/Results/Results.tsx
@@ -21,6 +21,18 @@ const Results: React.FC<Props> = ({ equations, results, setGameMode}) => {
   
   const navigate = useNavigate();
 
+
+  const setColor:(index: number) => string =  (index: number) => {
+    const equation = equations[index];
+    const result = results[index];
+    console.log(equation);
+    console.log(result);
+    if (eval(equation) == result) 
+      return "greenyellow";
+    else
+      return "red";
+  }
+
   return (
     <div className="results_container">
       <div className="results_header">
@@ -32,7 +44,7 @@ const Results: React.FC<Props> = ({ equations, results, setGameMode}) => {
           return (
             <>
               <span key={index}>{equation}</span>
-              <span key={1000 + index}>{results[index]}</span>
+              <span key={1000 + index} style={{color: setColor(index)}}>{results[index]}</span>
             </>
           );
         })}
